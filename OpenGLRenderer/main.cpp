265072e040b17ca9c74915f9e0d197d293327a7e
Lukas Kuhn <lukaskuhn-lku@gmail.com>
2022-10-27 20:29:58 +0200

    Draw Triangle and Switch Test

--- OpenGLRenderer/main.cpp
+++ OpenGLRenderer/main.cpp
@@ -7,6 +7,21 @@
 #include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_impl_opengl3.h>
 #include <imgui/imgui_impl_opengl3_loader.h>
+#include "main.h"
+
+const char* vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+                                 "out vec4 FragColor; \n"
+                                 "void main()"
+                                 "{\n"
+                                    "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+                                 "}\0";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -18,6 +33,32 @@ void processInput(GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
+
+void checkShaderCompileErrors(int shaderId, std::string shaderName) {
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shaderId, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(shaderId, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::"<<  shaderName << "::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+}
+
+void checkShaderProgramErrors(int programId) {
+    int success;
+    char infoLog[512];
+    glGetProgramiv(programId, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetProgramInfoLog(programId, 512, NULL, infoLog);
+        std::cout << "ERROR::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+}
+
 
 int main() {
     glfwInit();
@@ -40,43 +81,124 @@ int main() {
         return -1;
     }
 
+
+    // -----------  IMGUI SETUP ----------------
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     ImGui::StyleColorsDark();
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 330");
+    // ----------- END IMGUI SETUP -------------
 
 
     glViewport(0, 0, 800, 600);
-
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 
+
+    //VERTICES
     float vertices[] = {
     -0.5f, -0.5f, 0.0f,
      0.5f, -0.5f, 0.0f,
      0.0f,  0.5f, 0.0f
     };
 
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
     unsigned int VBO;
+    glBindVertexArray(VAO);
+
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+    //VERTICES 2
+    float vertices2[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.2f, 0.0f,
+     0.0f,  0.5f, 0.0f
+    };
+    
+    unsigned int VAO2;
+    glGenVertexArrays(1, &VAO2);
+
+    unsigned int VBO2;
+    glBindVertexArray(VAO2);
+
+    glGenBuffers(1, &VBO2);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+    //VERTEX SHADER
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    checkShaderCompileErrors(vertexShader, "VERTEX");
+
+
+    //FRAGMENT SHADER
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    checkShaderCompileErrors(fragmentShader, "FRAGMENT");
+
+
+    //SHADER PROGRAM
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    checkShaderProgramErrors(shaderProgram);
+
+    glUseProgram(shaderProgram);
+
+    //Cleanup Shaders after linking
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+    
+
+    bool checks = false;
+
     while (!glfwWindowShouldClose(window))
     {
+        if (checks) {
+            glBindVertexArray(VAO2);
+        }
+        else {
+            glBindVertexArray(VAO);
+        }
+
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        ImGui::Begin("Window");
-        ImGui::Text("Hello there");
+        ImGui::Begin("Selection");
+        ImGui::Checkbox("VBO Switch", &checks);
         ImGui::End();
 
 
